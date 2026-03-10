25af982022769b6e715e1f21f0a45b62f77ef9d4
Hugo <hugomagrimariutti07@gmail.com>
2026-03-09 22:39:48 -0300

    SegundaAULA

---
+++ SegundaAula.py
@@ -0,0 +1,62 @@
+
+#testando
+
+print("Manoela do pycharm")
+
+check1 = 8
+check2 = 6.5
+
+print(type(check1))
+print(type(check2))
+
+print(check1 + check2)
+
+soma = check1 + check2
+
+res = soma/2
+print(soma)
+print(res)
+
+#Exercicios
+
+#1
+
+print("Hugo Magri Mariutti")
+
+#2
+a = 3
+b = 5
+resultado= 2*a*3*b
+print(resultado,"\n")
+
+#3
+
+n1 = 30
+n2 = 10
+n3 = 15
+
+calculo = n1*n2*n3
+print(calculo)
+
+
+#teste
+
+pi = 3.14159265
+
+print(f"{pi:.2f}")
+print(f"{pi:.1f}")
+
+nome = "Hugo"
+print(type(nome))
+
+print(10 % 3)#resto da divisão 10/3
+print(10//3)#valor inteiro
+
+# Operadores lógicos
+
+check1 = float(input("CheckPoint 1 ="))
+check2 = float(input("CheckPoint 2 ="))
+
+media = (check1+check2)/2
+
+print(f"Média = {media:.1f}")
